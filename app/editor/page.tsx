e5d9ed6bacc89a81b1e6977c7137c966c346c5cf
Andrés Leiva <aleiva1700@gmail.com>
2023-12-06 09:52:54 -0500

    feat(home): add content description & editor link

---
+++ app/editor/page.tsx
@@ -0,0 +1,11 @@
+import {ReactElement} from "react";
+
+const EditorPage = (): ReactElement => {
+  return (
+    <main className="flex min-h-screen items-center justify-center dark:text-white">
+      <p>Editor</p>
+    </main>
+  );
+}
+
+export default EditorPage;
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
+import Link from "next/link";
 
 const HomePage = () => {
   return (
-    <main className="flex min-h-screen items-center justify-center dark:text-white">
-      <h1 className="text-9xl font-bold gradient-text drop-shadow-lg">GPC Algorithms</h1>
+    <main className="flex flex-col min-h-screen items-center justify-center dark:text-white">
+      <div className="text-center px-6">
+        <h1 className="text-9xl font-bold gradient-text drop-shadow-lg">GPC Algorithms</h1>
+        <p className="text-gray-500 dark:text-white text-lg font-medium mt-12">Aprende y conoce más sobre técnicas, algoritmos y estructuras de datos para que puedas mejorar tus resultados en contests de programación competitiva.</p>
+      </div>
+      <p className="absolute text-center text-gray-500 dark:text-white text-sm bottom-12">
+        ¿Quieres redactar contenido para GPC Algorithms? <Link href="/editor" className="hover:underline hover:font-medium underline-offset-4">Accede al editor</Link>
+      </p>
     </main>
   )
 }
